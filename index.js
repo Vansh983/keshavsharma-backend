7d6c186568b7a7ed985c75df35b3b6ef1168e85b
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2020-12-19 13:19:14 +0000

    Stripe

--- index.js
+++ index.js
@@ -1,117 +1,103 @@
-const functions = require('firebase-functions');
-require('dotenv/config');
-const cors = require("cors");
-const express = require("express");
-const stripe = require("stripe")(process.env.PRIIVATE_KEY);
+// const functions = require('firebase-functions');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const express = require('express');
+const stripe = require('stripe')(process.env.PRIVATE_KEY);
 const { v4: uuidv4 } = require('uuid');
 const nodemailer = require('nodemailer');
 
-
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
-app.use(cors({ origin: true }));
 
-// response.set('Access-Control-Allow-Origin', '*');
-
-app.get("/hello", (req, res) => {
-  res.send("You are at wrong location. Please check the link you are trying to access");
+app.get('/hello', (req, res) => {
+  res.send(
+    'You are at wrong location. Please check the link you are trying to access'
+  );
 });
 
-app.post("/xxuigjye", async (req, res) => {
-//   console.log("Request:", req.body);
-
-  let error;
-  let status;
+app.post('/xxuigjye', async (req, res) => {
   try {
-    const { product, token, contact } = req.body;
+    const { product, contact } = req.body;
 
     const customer = await stripe.customers.create({
-      email: token.email,
-      source: token.id
+      email: contact.email,
+      name: contact.name,
+      phone: contact.phone,
     });
-
-    // console.log(contact);
 
     const output = `
       <h1>A new payment was made on Aumkeshav Sharma</h1>
       <h3>Contact Details:</h3>
       <ul>
-        <li>Name: ${contact.Name}</li>
-        <li>Email: ${contact.Email}</li>
-        <li>Phone: ${contact.Phone}</li>
+        <li>Name: ${contact.name}</li>
+        <li>Email: ${contact.email}</li>
+        <li>Phone: ${contact.phone}</li>
         <li>Paid for: ${product.name}</li>
       </ul>
     `;
 
-    // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: process.env.MAIL_USERNAME, // generated ethereal user
-      pass: process.env.MAIL_PASSWORD, // generated ethereal password
-    },
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+    // let transporter = nodemailer.createTransport({
+    //   host: process.env.MAIL_HOST,
+    //   port: process.env.MAIL_PORT,
+    //   secure: true, // true for 465, false for other ports
+    //   auth: {
+    //     user: process.env.MAIL_USERNAME, // generated ethereal user
+    //     pass: process.env.MAIL_PASSWORD, // generated ethereal password
+    //   },
+    //   tls: {
+    //     rejectUnauthorized: false,
+    //   },
+    // });
 
-    const idempotency_key = uuidv4();
-    const charge = await stripe.charges.create(
-      {
-        amount: product.price * 100,
-        currency: "usd",
-        customer: customer.id,
-        receipt_email: token.email,
-        description: `Purchased the ${product.name}`,
-        shipping: {
-          name: token.card.name,
-          address: {
-            line1: token.card.address_line1,
-            line2: token.card.address_line2,
-            city: token.card.address_city,
-            country: token.card.address_country,
-            postal_code: token.card.address_zip
-          }
-        }
+    const session = await stripe.checkout.sessions.create({
+      customer: customer.id,
+      billing_address_collection: 'required',
+      payment_method_types: ['card'],
+      metadata: {
+        time: product.time,
       },
-      {
-        idempotency_key
-      }
-    );
-    console.log("Charge:", { charge });
-    status = "success";
-    if (status === "success") {
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
-        from: '"Payment Confirmation" <paymentconfirm@aumkeshavsharma.com>', // sender address
-        to: 'payment@aumkeshavsharma.com', // list of receivers
-        subject: "A payment was made on our website", // Subject line
-        text: "Hello world?", // plain text body
-        html: output, // html body
-      });
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: `${product.name} - ${product.time}`,
+            },
+            unit_amount: product.price * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${process.env.FRONTEND_URL}/?payment=success`,
+      cancel_url: process.env.FRONTEND_URL,
+    });
 
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    // let info = await transporter.sendMail(
+    //   {
+    //     from: '"Payment Confirmation" <paymentconfirm@aumkeshavsharma.com>',
+    //     to: 'payment@aumkeshavsharma.com',
+    //     subject: 'A payment was made on our website',
+    //     text: 'Hello woIrld?',
+    //     html: output,
+    //   },
+    //   () => {}
+    // );
+    // console.log('Message sent: %s', info.messageId);
+    // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-    }
+    res.json({ id: session.id, status: 'success' });
   } catch (error) {
-    console.error("Error:", error);
-    status = "failure";
+    console.error('Error:', error);
+    res.json({ status: 'fail' });
   }
-
-  res.json({ error, status });
 });
 
-// console.log(process.env.PRIIVATE_KEY);
-// console.log(process.env.MAIL_HOST);
-// console.log(process.env.MAIL_PORT);
-// console.log(process.env.MAIL_PASSWORD);
-
-exports.app = functions.https.onRequest(app);
+// exports.app = functions.https.onRequest(app);
+app.listen(5000, () => {
+  console.log('Server running');
+});
